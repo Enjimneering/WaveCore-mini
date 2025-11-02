fb74e764ccf7b6dacb9393becc02863aa1e3f966
James <162901067+Enjimneering@users.noreply.github.com>
2025-11-02 14:25:09 +0000

    Basic testbench

--- test/test.py
+++ test/test.py
@@ -1,40 +1,27 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
+from random import randint
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, RisingEdge
 
+async def reset(uut, reset_duration=randint(1,10)):
+    # assert reset
+    uut._log.info("Resetting Module")
+    uut.rst_n.value = 0
+    await ClockCycles(uut.clk, reset_duration)
+    uut.rst_n.value = 1
 
 @cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
+async def test_apu_sanity(uut):
+    # start clock
+    clock = Clock(uut.clk, 40, unit="ns")
     cocotb.start_soon(clock.start())
+    await ClockCycles(uut.clk, 1)
+   
+    # reset the module
+    await reset(uut)
+    await RisingEdge(uut.clk)
 
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+    # continue test ...
+    await ClockCycles(uut.clk, 100)
+    uut._log.info("Test Complete!")
 
-    dut._log.info("Test project behavior")
-
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
