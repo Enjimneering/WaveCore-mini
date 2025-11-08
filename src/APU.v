cf7dd8d4ce023e6d833343b098618f7d5d8304e7
James <162901067+Enjimneering@users.noreply.github.com>
2025-11-08 10:56:13 +0000

    Add Envelopes

--- src/APU.v
+++ src/APU.v
@@ -51,18 +51,18 @@ module AudioProcessingUnit (
     .counter_we(counter_we)
   );
 
-  reg[1:0] trig_count;
+  reg[2:0] trig_count;
 
   always @(posedge clk) begin
     if (reset) begin
       trig_count <= 0;
       square_reg <= 0;
     end else begin
-        if (trigger) begin
-          trig_count <= trig_count + 1;
-        if (trig_count == 2'b01) 
-          square_reg <= ~square_reg;
-        end 
+      if (trigger) begin
+        trig_count <= trig_count + 1;
+      if (trig_count == 3'b111) 
+        square_reg <= ~square_reg;
+      end 
     end
   end
 
@@ -76,18 +76,11 @@ module AudioProcessingUnit (
     end
   end
 
-  // lsfr  
-  reg [12:0] lfsr = 13'h0e1f;
-  wire feedback = lfsr[12] ^ lfsr[8] ^ lfsr[2] ^ lfsr[0] + 1;
+  reg [15:0] pwm_counter = 0; // PWM timebase counter
+  reg saw_pwm_out;
+  reg lfsr_pwm_out;
+
   always @(posedge clk) begin
-    lfsr <= {lfsr[11:0], feedback};
-  end
-
-reg [15:0] pwm_counter = 0; // PWM timebase counter
-reg saw_pwm_out;
-reg lfsr_pwm_out;
-
-always @(posedge clk) begin
     if (reset) begin
         pwm_counter <= 0;
         saw_pwm_out <= 0;
@@ -99,12 +92,54 @@ always @(posedge clk) begin
         saw_pwm_out  <= (pwm_counter < counter_reg);
         lfsr_pwm_out <= (pwm_counter[12:0] < lfsr);
     end
-end
+  end
 
-wire saw    = SheepDragonCollision & saw_pwm_out;
-wire noise  = PlayerDragonCollision & lfsr_pwm_out;
-wire square = SwordDragonCollision & square_reg;
 
-assign sound = saw + noise + square;
+  // lsfr  
+  reg [12:0] lfsr = 13'h0e1f;
+  wire feedback = lfsr[12] ^ lfsr[8] ^ lfsr[2] ^ lfsr[0] + 1;
+  always @(posedge clk) begin
+    lfsr <= {lfsr[11:0], feedback};
+  end
+
+  // envelopes and timer
+  wire [11:0] timer = frame_counter;
+  reg noise_reg, noise_src = ^lfsr;
+  reg  [2:0] noise_counter;
+  
+  wire [4:0] envelopeA = 5'd31 - timer[4:0];   // exp(t*-10) decays to 0 approximately in 32 frames  [255 215 181 153 129 109  92  77  65  55  46  39  33  28  23  20  16  14 12  10   8   7   6   5   4   3   3   2   2]
+  wire [4:0] envelopeB = 5'd31 - timer[3:0]*2; // exp(t*-20) decays to 0 approximately in 16 frames  [255 181 129  92  65  46  33  23  16  12   8   6   4   3]
+
+  reg [11:0] frame_counter;
+  always @(posedge clk) begin
+    if (reset) begin
+      frame_counter <= 0;
+      noise_counter <= 0;
+      noise_reg <= 0;
+    end else begin
+
+      // frame counter
+      if (x == 0 && y == 0) begin
+        frame_counter <= frame_counter + 1;
+      end
+
+      // noise
+      if (x == 0) begin
+        if (noise_counter > 1) begin 
+          noise_counter <= 0;
+          noise_reg <= noise_reg ^ noise_src;
+        end else begin
+          noise_counter <= noise_counter + 1'b1;
+        end
+      end
+
+    end
+  end
+
+  wire saw    = SheepDragonCollision  & saw_pwm_out & (x < envelopeA*8);
+  wire noise  = PlayerDragonCollision & noise_reg   & (x >= 128 && x < 128+envelopeB*4);
+  wire square = SwordDragonCollision  & square_reg  & (x < envelopeA*4); ;
+
+  assign sound = saw + noise + square;
 
 endmodule
